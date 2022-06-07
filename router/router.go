f2f543ab60e5bcd650db3be633697f3946c24c2a
Loklak <angelo.boutros.a@gmail.com>
2022-06-07 16:51:33 +0200

    Move midddleware into specific file in package router

--- router/router.go
+++ router/router.go
@@ -9,20 +9,6 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func validatingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Do stuff here
-		if r.Header.Get("Content-Type") != "application/json" {
-			log.Println("Invalid content type")
-			http.Error(w, "Invalid content type", http.StatusBadRequest)
-			return
-		}
-		log.Println(r.RequestURI)
-		// Call the next handler, which can be another middleware in the chain, or the final handler.
-		next.ServeHTTP(w, r)
-	})
-}
-
 func InitServer(port int) {
 
 	logger.InfoLogger.Println("Starting server on port", port)
